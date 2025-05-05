0f8b8dd67c774d8c6c3547ef4e272b4fab7d9ff9
Christina <xtina.lang.25@gmail.com>
2025-05-05 16:30:46 -0400

    messing around with linting and black.

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -8,9 +8,9 @@ logging.basicConfig(
 
 logger = logging.getLogger("weather_app")
 
-# # Example logs
-# # logger.debug("Debug message")
-# # logger.info("Info message")
-# # logger.warning("Warning message")
-# # logger.error("Error message")
-# # logger.critical("Critical message")
+# Example logs
+# logger.debug("Debug message")
+# logger.info("Info message")
+# logger.warning("Warning message")
+# logger.error("Error message")
+# logger.critical("Critical message")
