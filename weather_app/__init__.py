7e9ccb10326c37641f4b6bd68fbd5e134b90706a
Christina <xtina.lang.25@gmail.com>
2025-05-05 08:44:48 -0400

    adding logging

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,9 +1,16 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("weather_app.log"), logging.StreamHandler()],
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("weather_app.log")],
 )
 
 logger = logging.getLogger("weather_app")
+
+# # Example logs
+# # logger.debug("Debug message")
+# # logger.info("Info message")
+# # logger.warning("Warning message")
+# # logger.error("Error message")
+# # logger.critical("Critical message")
